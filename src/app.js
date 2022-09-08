23f76c9a31f34dffefee3ca82a90f7af29cb00a5
Douglas Ward <douglasward@Douglass-MacBook-Air.local>
2022-09-08 07:38:11 -0700

    Run Prettier

--- src/app.js
+++ src/app.js
@@ -1,63 +1,69 @@
-require('dotenv').config();
-const SunCalc = require('suncalc')
-const schedule = require('node-schedule');
+require("dotenv").config();
+const SunCalc = require("suncalc");
+const schedule = require("node-schedule");
 
-const Hue = require('./hue')
+const Hue = require("./hue");
 
-const main = async () => { 
-    const hueAPI = await Hue.build()
+/**
+ * TODO:
+ *  * Test the damn thing out.
+ *  * Make the routines attributes of the Hue class.
+ *  * Make routines use the 'groups' API, rather than write to each bulb individually.
+ * NICE TO HAVE:
+ *  * A propper logger would help with debugging.
+ */
+const main = async () => {
+  const hueAPI = await Hue.build();
 
-    const getSunTimes = () => {
-        // Hardcoded for Seattle, for now
-        const times = SunCalc.getTimes(new Date(), 47, -122);
-        const sunrise = times.sunrise
-        const sunset = times.sunset // .getHours() + ':' + times.sunrise.getMinutes();
+  const getSunTimes = () => {
+    // Hardcoded for Seattle, for now
+    const { sunrise, sunset } = SunCalc.getTimes(new Date(), 47, -122);
 
-        const sunriseStart = new Date(sunrise)
-        sunriseStart.setMinutes(sunrise.getMinutes() - 30)
-        const sunsetStart = new Date(sunset)
-        sunsetStart.setMinutes(sunset.getMinutes() - 30)
+    const sunriseStart = new Date(sunrise);
+    sunriseStart.setMinutes(sunrise.getMinutes() - 30);
+    const sunsetStart = new Date(sunset);
+    sunsetStart.setMinutes(sunset.getMinutes() - 30);
 
-        return [sunriseStart, sunsetStart]
-    }
+    return [sunriseStart, sunsetStart];
+  };
 
-    const initialize = async () => {
-        
-        const [sunrise, sunset] = getSunTimes()
+  const initialize = async () => {
+    const [sunrise, sunset] = getSunTimes();
 
-        // Set schedule to check for sunset times every day at midnight
-        schedule.scheduleJob('0 0 * * *', () => {
-            [sunrise, sunset] = getSunTimes()
-            console.log('ran schedule now', new Date())
-        })
-        return [sunrise, sunset]
-    }
-    
-    const sunsetRoutine = async () => {
-        // Get all lights that are on
-        const lights = (await hueAPI.getLights()).filter(light => 
-            light.state.on
-        )
-        lights.forEach(light => {
-            if (light.state.ct) {
-                hueAPI.setLightState(light.id, {ct: 500, transitiontime: 36000})
-            } else {
-                hueAPI.setLightState(light.id, {hue: 11342, sat: 183, transitiontime: 36000})
-            }
-            
-        })
-    }
+    // Set schedule to check for sunset times every day at midnight
+    schedule.scheduleJob("0 0 * * *", () => {
+      [sunrise, sunset] = getSunTimes();
+      console.log("ran schedule now", new Date());
+    });
+    return [sunrise, sunset];
+  };
 
-    let [sunriseStart, sunsetStart] = await initialize()
+  const sunsetRoutine = async () => {
+    // Get all lights that are on
+    const lights = (await hueAPI.getLights()).filter((light) => light.state.on);
+    lights.forEach((light) => {
+      if (light.state.ct) {
+        hueAPI.setLightState(light.id, { ct: 500, transitiontime: 36000 });
+      } else {
+        hueAPI.setLightState(light.id, {
+          hue: 11342,
+          sat: 183,
+          transitiontime: 36000,
+        });
+      }
+    });
+  };
 
-    // For testing purposes
-    const now = new Date()
-    now.setMilliseconds(now.getMilliseconds() + 2000 )
+  let [sunriseStart, sunsetStart] = await initialize();
 
-    const job = schedule.scheduleJob(sunsetSTart, () => {
-        console.log('ran sunset routine now')
-        sunsetRoutine()
-    })
-}
+  // For testing purposes
+  const now = new Date();
+  now.setMilliseconds(now.getMilliseconds() + 2000);
 
-main()
+  const job = schedule.scheduleJob(sunsetStart, () => {
+    console.log("ran sunset routine now");
+    sunsetRoutine();
+  });
+};
+
+main();
--- src/hue.js
+++ src/hue.js
@@ -1,55 +1,49 @@
-const v3 = require('node-hue-api').v3
-    , hueApi = v3.api
-    , Rule = v3.rules.Rule
-    , LightState = v3.lightStates.LightState;
+const v3 = require("node-hue-api").v3,
+  hueApi = v3.api,
+  Rule = v3.rules.Rule,
+  LightState = v3.lightStates.LightState;
 
 class Hue {
-    constructor(ipAddress, api, username) {
-        this.IP_ADDRESS = ipAddress
-        this.api = api
-        this.USERNAME = username
+  constructor(ipAddress, api, username) {
+    this.IP_ADDRESS = ipAddress;
+    this.api = api;
+    this.USERNAME = username;
+  }
+
+  static async build() {
+    const username = process.env.USERNAME;
+    const ipAddress = await this.getBridgeIp();
+    const api = await this.getApi(ipAddress, username);
+    return new Hue(ipAddress, api, username);
+  }
+
+  static getBridgeIp = async () => {
+    const results = await v3.discovery.nupnpSearch();
+    if (results.length !== 0) {
+      return results[0].ipaddress;
     }
+  };
 
-    static async build () {
-        const username = process.env.USERNAME
-        const ipAddress = await this.getBridgeIp()
-        const api = await this.getApi(ipAddress, username)
-        return new Hue(ipAddress, api, username)
-    }
+  static getApi = async (ipAddress, username) => {
+    const api = await hueApi.createLocal(ipAddress).connect(username);
+    return api;
+  };
 
-    static getBridgeIp = async () => {
-        const results = await v3.discovery.nupnpSearch()
-        if (results.length !== 0) {
-            return results[0].ipaddress
-        }
-    }
+  async getLights() {
+    const results = await this.api.lights.getAll();
+    const lights = results.map((light) => {
+      return light["_data"];
+    });
+    return lights;
+  }
 
-    static getApi = async (ipAddress, username) => {
-        const api = await hueApi.createLocal(ipAddress).connect(username)
-        return api
-    }
+  async setLightState(lightId, { hue, sat, ct, transitiontime }) {
+    const lightState = ct
+      ? new LightState().on().ct(ct).transitiontime(transitiontime)
+      : new LightState().on().hue(hue).sat(sat).transitiontime(transitiontime);
 
-    async getLights () {
-        const results = await this.api.lights.getAll()
-        const lights = results.map(light => {
-            return light["_data"]
-        })
-        return lights
-    } 
-
-    async setLightState (lightId, {hue, sat, ct, transitiontime}) {
-        const lightState = ct ? new LightState()
-                .on()
-                .ct(ct)
-                .transitiontime(transitiontime) 
-            : new LightState()
-                .on()
-                .hue(hue)
-                .sat(sat)
-                .transitiontime(transitiontime)
-
-        return this.api.lights.setLightState(lightId, lightState)
-    }
+    return this.api.lights.setLightState(lightId, lightState);
+  }
 }
 
-module.exports = Hue
+module.exports = Hue;
